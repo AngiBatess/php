51e9be0bfeb123cacfaa7e01cfa24d4d3ff37b51
Angila Bates <iu8429rb@my.hennepintech.edu>
2019-02-20 12:14:13 -0600

    first

---
+++ menu.php
@@ -0,0 +1,7 @@
+<p>
+  <a href="index.php">Index Page</a>
+  <a href="page2.php">Page 2</a>
+  <a href="page3.php">Page 3</a>
+  <a href="page4.php">Page 4</a>
+  <a href="page5.php">Page 5</a>
+</p>
---
+++ page2.php
@@ -0,0 +1,24 @@
+<?php
+include 'menu.php';
+echo '<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Angie\'s Page 2"</h1>
+
+<p>My first paragraph.</p>
+
+</body>
+</html>
+';
+
+
+
+
+
+
+
+
+
+
+>
